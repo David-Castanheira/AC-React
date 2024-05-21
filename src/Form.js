c1ea4c600160c62914d688f06bc898e2cc3e875f
David Castanheira de Souza <85026767+David-Castanheira@users.noreply.github.com>
2024-05-21 09:58:37 -0300

    Inclusão dos campos no formulário em React

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 
 function Form() {
     const [campos, setCampos] = useState({
-        txtNome: ''
+        txtNome: '',
+        txtEndereco: '', 
+        txtTelefone: ''
     });
 
     const [ret, setRet] = useState('');
@@ -34,8 +36,12 @@ function Form() {
                         <h2>Formulário de cadastro</h2>
                     </legend>
                     <div>
-                        <label>Nome:</label>
-                        <input type='text' name='txtNome' id='txtNome' onChange={handleInputChange}></input>
+                        <label>Nome</label>
+                        <input type='text' name='txtNome' id='txtNome' onChange={handleInputChange}></input><br />
+                        <label>Endereço</label>
+                        <input type='text' name='txtEndereco' id='txtEndereco' onChange={handleInputChange}></input><br />
+                        <label>Telefone</label>
+                        <input type='text' name='txtTelefone' id='txtTelefone' onChange={handleInputChange}></input><br />
                     </div>
                     <input type='submit' value='Salvar'></input>
                     <div><p>Retornou: {ret}</p></div>
